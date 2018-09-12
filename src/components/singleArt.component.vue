6bba99ade9bcaca5f2d69ee771063e37e91edb8d
unknown <surferbum618@ymail.com>
2018-09-12 18:33:37 -0500

    Added logic to update and edit blog posts in Firebase database. Still need to have the function pull the current value in the text area element.

--- src/components/singleArt.component.vue
+++ src/components/singleArt.component.vue
@@ -6,17 +6,26 @@
       <button type="button" v-on:click="editFn()">Edit</button>
     </div>
     <div v-else>
-      <textarea class="editBox">{{blog.title}}</textarea>
-      <textarea class="editBox">{{blog.content}}</textarea>
-      <button type="button" v-on:click="editFn()">Edit</button>
+      <div class="row">
+        <textarea rows="1" cols="15" class="editBox" v-model="blog.title"></textarea>
+      </div>
+      <div class="row">
+        <textarea rows="10" cols="70" class="editBox" v-model="blog.content"></textarea>
+      </div>
+
+      <button type="button" v-on:click="updateFn()">Update</button>
     </div>
   </div>
 </template>
 
 <style lang="css">
+.editBox {
+  margin: 0 auto;
+}
 </style>
 
 <script>
+import firebase from 'firebase'
 export default {
   data(){
     return {
@@ -28,6 +37,12 @@ export default {
   methods: {
     editFn: function(){
         return this.posted = !this.posted
+      },
+      updateFn: function(){
+        firebase.database().ref('posts/' + this.id).set({
+          title: "",
+          content: ""
+        })
       }
   },
   created(){
--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import VueRouter from 'vue-router'
 import routes from './router'
 import VueResource from 'vue-resource'
+import firebase from 'firebase'
 
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 Vue.use(VueResource);
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAkEWcFXwkOSK_aM_amtq2RqL3jJEPNeWE",
+    authDomain: "workylabtecnico.firebaseapp.com",
+    databaseURL: "https://workylabtecnico.firebaseio.com",
+    projectId: "workylabtecnico",
+    storageBucket: "workylabtecnico.appspot.com",
+    messagingSenderId: "1000188479094"
+  };
+  firebase.initializeApp(config);
 
 const router = new VueRouter({ routes: routes })
 
